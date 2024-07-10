67b1daf4784670f3e2c7d3411e459797689e3d60
aryan1403 <aaryan14032006@email.com>
2024-07-10 13:00:21 +0530

    meow

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-
-const mongo_uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/authtest';
+require('dotenv').config()
+const mongo_uri = process.env.MONGO_URI;
 mongoose
     .connect(mongo_uri)
     .then(() => console.log("Connected"))
